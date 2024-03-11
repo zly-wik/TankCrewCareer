3c6167fddf3cf04a621069eebdd913b15ff660a9
Zly Wik <wiktor.dlugi.bader@gmail.com>
2024-03-11 17:36:44 +0100

    Fixed imports in core/urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,10 @@
 from rest_framework.routers import SimpleRouter
 
-from core.views import MissionObjectVS, MissionOptionsVS, VehicleVS
+from core.views import MissionObjectViewSet, MissionOptionsViewSet, VehicleViewSet
 
 router = SimpleRouter()
-router.register('missions', MissionOptionsVS, basename='missions')
-router.register('mission-objs', MissionObjectVS, basename='mission-objs')
-router.register('vehicles', VehicleVS, basename='vehicles')
+router.register('missions', MissionOptionsViewSet, basename='missions')
+router.register('mission-objs', MissionObjectViewSet, basename='mission-objs')
+router.register('vehicles', VehicleViewSet, basename='vehicles')
 
 urlpatterns = router.urls
